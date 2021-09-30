f93794170cc24c24884be7d17745a7c818671826
Kalyxt <kalixt001@gmail.com>
2021-09-30 14:15:37 +0100

    init

---
+++ MAUITester_CarouselView/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,11 @@
+﻿using Foundation;
+using Microsoft.Maui;
+
+namespace MAUITester_CarouselView
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
